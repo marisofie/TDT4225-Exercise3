26d436753ace2be63b0ce0ffb7c58899b1f83079
Mari Sofie Lerfaldet <marilerfaldet@gmail.com>
2020-10-14 11:16:17 +0200

    set up database connection with .env and gitignore

---
+++ DbConnector.py
@@ -0,0 +1,42 @@
+from pymongo import MongoClient, version
+import os
+from dotenv import load_dotenv
+
+
+class DbConnector:
+    """
+    Connects to the MongoDB server on the Ubuntu virtual machine.
+    Connector needs HOST, USER and PASSWORD to connect.
+
+    Example:
+    HOST = "tdt4225-00.idi.ntnu.no" // Your server IP address/domain name
+    USER = "testuser" // This is the user you created and added privileges for
+    PASSWORD = "test123" // The password you set for said user
+    """
+
+    BASEDIR = os.path.abspath(os.path.dirname(__file__))
+    load_dotenv(os.path.join(BASEDIR, '.env'))
+
+    def __init__(self,
+                 DATABASE='db_geodata',
+                 HOST="tdt4225-37.idi.ntnu.no",
+                 USER="group37",
+                 PASSWORD= os.getenv('PASSWORD')):
+        uri = "mongodb://%s:%s@%s/%s" % (USER, PASSWORD, HOST, DATABASE)
+        # Connect to the databases
+        try:
+            self.client = MongoClient(uri)
+            self.db = self.client[DATABASE]
+        except Exception as e:
+            print("ERROR: Failed to connect to db:", e)
+
+        # get database information
+        print("You are connected to the database:", self.db.name)
+        print("-----------------------------------------------\n")
+
+    def close_connection(self):
+        # close the cursor
+        # close the DB connection
+        self.client.close()
+        print("\n-----------------------------------------------")
+        print("Connection to %s-db is closed" % self.db.name)
