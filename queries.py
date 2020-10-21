a0c6050d826dc2623fa9ea8a9e8cf7954c4dbd83
Jon Ryfetten <jonry@stud.ntnu.no>
2020-10-21 15:46:20 +0200

    Completed task 2,5,8 and 11

--- queries.py
+++ queries.py
@@ -3,6 +3,7 @@ from pprint import pprint
 from datetime import datetime
 from haversine import haversine
 
+
 class Query:
     def __init__(self):
         self.connection = DbConnector()
@@ -17,10 +18,39 @@ class Query:
             count = col.count()
             print(f"Collections {collection} has {count} documents.")
 
+    def q2(self):
+        activities = self.db["Activity"].count_documents({})
+        users = self.db["User"].count_documents({})
+        avg = activities / users
+        print("The average number of activities per user is: {}".format(round(avg, 0), float))
+
     def q4(self):
         collection = self.db["Activity"]
         user_ids = collection.distinct("user_id", {"transportation_mode": "taxi"})
-        print("The following users have taken a taxi:",user_ids)
+        print("The following users have taken a taxi:", user_ids)
+
+    def q5(self):
+        collection = self.db["Activity"]
+        types = collection.aggregate([
+            {
+                "$match": {
+                    "transportation_mode": {
+                        "$exists": True,
+                        "$ne": "null",
+                    }
+                }
+            },
+            {
+
+                "$group": {
+                    "_id": "$transportation_mode",
+                    "count": {"$sum": 1}
+                }
+            }
+        ])
+        print("Activities per transport mode:")
+        for data in list(types):
+            print(" - {}: {}".format(data["_id"], data["count"]))
 
     def q7(self):
         activity_col = self.db["Activity"]
@@ -43,13 +73,76 @@ class Query:
         total_dist = 0
         for activity in activities:
             trackpoints = activity["trackpoints"]
-            for i in range(len(trackpoints)-1):
+            for i in range(len(trackpoints) - 1):
                 coord1 = (float(trackpoints[i]["lat"]), float(trackpoints[i]["lon"]))
-                coord2 = (float(trackpoints[i+1]["lat"]), float(trackpoints[i+1]["lon"]))
+                coord2 = (float(trackpoints[i + 1]["lat"]), float(trackpoints[i + 1]["lon"]))
                 dist = haversine(coord1, coord2)
                 total_dist += dist
 
         print(f"Total distance walked by user 112: {total_dist}")
+
+    def q8(self):
+        collection = self.db["TrackPoint"]
+        # collection.ensure_index("_id", 1)
+        meters = collection.aggregate([
+            {
+                "$match": {
+                    "user_id": {
+                        "$exists": True,
+                        "$ne": "null",
+                    },
+                    "altitude": {
+                        "$exists": True,
+                        "$ne": "null",
+                    }
+                }
+            },
+            {
+                "$addFields": {
+                    "prevTPID": {"$subtract": ["$_id", 1]}
+                }
+            },
+            {
+                "$sort": {
+                    "_id": -1,
+                }
+            },
+            {
+                "$lookup": {
+                    "from": "TrackPoint",
+                    "localField": "prevTPID",
+                    "foreignField": "_id",
+                    "as": "t2"
+                }
+            },
+            {
+                "$project": {
+                    "user_id": 1,
+                    "delta": {"$subtract": ["$altitude", {"$sum": "$t2.altitude"}]}
+                }
+            },
+            {
+                "$group": {
+                    "_id": '$user_id',
+                    "total": {
+                        "$sum": {
+                            "$cond": [
+                                {"$gt": ['$delta', 0]}, '$delta', 0
+                            ]
+                        }
+                    }
+                }
+            },
+            {
+                "$sort": {"total": -1}
+            },
+            {
+                "$limit": 20
+            }
+        ], allowDiskUse=True)
+        print("Top 20 users that gained most altitude:")
+        for data in list(meters):
+            print(" {}: {}".format(data["_id"], data["total"]))
 
     def q10(self):
         lat = 39.916
@@ -61,11 +154,70 @@ class Query:
                 "lon": {"$lt": lon + 0.0005, "$gte": lon - 0.0005}
             }},
             {"$group": {
-               "_id": "$user_id",
+                "_id": "$user_id",
             }}
         ])
 
         print("The following users have been in the Forbidden City:")
         for data in user_ids:
             pprint(data["_id"])
+
+    def q11(self):
+        collection = self.db["Activity"]
+        data = collection.aggregate([
+            {
+                "$match": {
+                    "transportation_mode": {
+                        "$exists": True,
+                        "$ne": "null",
+                    }
+                }
+            },
+            {
+                "$group": {
+                    "_id": {
+                        "category": "$transportation_mode",
+                        "user": "$user_id"
+                    },
+                    "count": {
+                        "$sum": 1
+                    },
+                }
+            },
+
+            {
+                "$sort": {
+                    "count": -1,
+                    "_id.category": -1
+                },
+            },
+            {
+                "$group": {
+                    "_id": "$_id.user",
+                    "count": {
+                        "$first": "$count"
+                    },
+                    "category": {
+                        "$first": "$_id.category"
+                    }
+                }
+            },
+            {
+                "$project": {
+                    "_id": 0,
+                    "user_id": "$_id",
+                    "most_used_transportation_mode": "$category",
+                }
+            },
+            {
+                "$sort": {
+                    "user_id": 1
+                }
+            }
+        ])
+        print("Most common transportation mode for users (only users who have registered the transportation_mode)")
+        print(list(data))
+
+q = Query()
+q.q8()
 
