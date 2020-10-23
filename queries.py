3135f770065f7107c30f6bf6ba2056ede8dd538f
noravv <n.v.vaage@gmail.com>
2020-10-23 15:59:24 +0200

    Add queries 3, 6, 9

--- queries.py
+++ queries.py
@@ -15,13 +15,62 @@ class Query:
         for collection in collections:
             col = self.db[collection]
             count = col.count()
-            print(f"Collections {collection} has {count} documents.")
+            print("Collections {collection} has {count} documents.")
+
+    def q3(self):
+        """Find the top 20 users with the highest number of activities."""
+        activities = self.db["Activity"]
+        top20_users = activities.aggregate([
+            {"$group": {
+                "_id": "$user_id", "count": {"$sum": 1}
+            }},
+            {"$sort": {
+                "count": -1}
+            },
+            {"$limit": 20}
+
+        ])
+        print("Users with the most activities are:")
+        print("User  Activities")
+        for user in top20_users:
+            pprint(str(user["_id"]) + "  " + str(user["count"]))
 
     def q4(self):
         collection = self.db["Activity"]
         user_ids = collection.distinct("user_id", {"transportation_mode": "taxi"})
         print("The following users have taken a taxi:",user_ids)
 
+    def q6(self):
+        """a) Find the year with the most activities.
+           b) Is this also the year with most recorded hours?"""
+        activities = self.db["Activity"]
+        #a
+        activities_per_year = activities.aggregate([
+            {"$group" : {
+                "_id" : {"$year": "$start_date_time"},
+                "Activities" : {"$sum" : 1}
+            }},
+            {"$sort": {
+                "Activities": -1
+            }},
+            {"$limit": 1}
+        ])
+        for year in activities_per_year:
+            print("The year with the most activities was: " + str(year["_id"]))
+
+        #b
+        hours_per_year = activities.aggregate([
+            {"$group": {
+                "_id" : {"$year": "$start_date_time"},
+                "Hours" : {"$sum": {"$subtract":["$end_date_time", "$start_date_time"]} }
+            }},
+            {"$sort": {
+                "Activities": -1
+            }},
+            {"$limit": 1}
+        ])
+        for year in hours_per_year:
+            print("The year with the most hours was: " + str(year["_id"]))
     def q7(self):
         activity_col = self.db["Activity"]
         activities = activity_col.aggregate([
@@ -49,7 +98,56 @@ class Query:
                 dist = haversine(coord1, coord2)
                 total_dist += dist
 
-        print(f"Total distance walked by user 112: {total_dist}")
+        print("Total distance walked by user 112: {total_dist}")
+
+    def q9(self):
+        """Find all users who have invalid activities, and the number of invalid activities per
+        user
+        An invalid activity is defined as an activity with consecutive trackpoints
+        where the timestamps deviate with at least 5 minutes."""
+        collection = self.db["Activity"]
+        activity_trackpoints = collection.aggregate([
+            {"$lookup": {
+                "from": "TrackPoint",
+                "localField" : "_id",
+                "foreignField" : "activity_id",
+                "as" : "Activity"
+            }},
+            {"$project": {
+                "user_id" : "$user_id", "Activity" : "$Activity.activity_id" , "Time" : "$Activity.date_time"
+            }},
+            {"$limit" : 100}
+
+        ])
+
+        activity_trackpoints = list(activity_trackpoints)
+
+        previous_time = datetime.now()
+        previous_activity = 0
+        invalid_trackpoints = {}
+
+        for activity in activity_trackpoints:
+            current_activity = activity["Activity"]
+
+            for time in activity["Time"]:
+                current_time = time
+
+                if (current_activity == previous_activity) and (current_time - previous_time).total_seconds() > 5*60 :
+                    if activity["user_id"] not in invalid_trackpoints.keys():
+                        invalid_trackpoints[activity["user_id"]] = 1
+                    else:
+                        invalid_trackpoints[activity["user_id"]] += 1
+                    break
+                previous_time = current_time
+                previous_activity = current_activity
+
+
+        #for activity in activity_trackpoints:
+            #pprint(activity)
+        for user,value in invalid_trackpoints.items():
+            print(user, value)
+
+
 
     def q10(self):
         lat = 39.916
@@ -69,3 +167,9 @@ class Query:
         for data in user_ids:
             pprint(data["_id"])
 
+
+def main():
+    program = Query()
+    program.q9()
+
+main()
